b4855546f0f40d42958db23a48031f80e816af0c
Samuel <spantoja0208@gmail.com>
2023-03-10 16:03:27 +0100

    Now it encrypts with two keys

---
+++ CaesarsCypher/TwoKeysCypher.py
@@ -0,0 +1,15 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz" 
+
+def shift(string, key):
+    return string[key:] + string[:key]
+
+def encrypt(message, key1, key2):
+    result = ""
+    message = message.lower()
+    shifted1 = shift(alphabet, key1)
+    shifted2 = shift(alphabet, key2)
+    for i in range(len(message)):
+        index = alphabet.find(message[i])
+        result += shifted1[index] if i % 2 == 0 else shifted2[index]
+    return result
+
